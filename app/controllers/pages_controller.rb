2f44b9564422fb3d45fa24d128733a1324836a25
Nicholas Page <npage85@gmail.com>
2017-05-03 19:53:46 -0700

    Added secret area that only users can get into.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:secret]
+
   def home
+  end
+
+  def secret
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
+
+  get 'secret', to: 'pages#secret', as: :secret
 end
